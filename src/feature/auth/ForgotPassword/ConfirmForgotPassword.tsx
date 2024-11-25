61a15d7da5da51972b57cedbcdbfb4866e52e6b0
truongbqhe160611 <buitruong091102@gmail.com>
2024-11-26 01:10:34 +0700

    fixlast

--- src/feature/auth/ForgotPassword/ConfirmForgotPassword.tsx
+++ src/feature/auth/ForgotPassword/ConfirmForgotPassword.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ForgotPopUp from './ForgotPopUp';
 
 const ConfirmForgotPassword = () => {
     // Lấy token từ URL bất kể môi trường hiện tại
-    const currentUrl = window.location.href; // URL đầy đủ, ví dụ: https://argonaut.asia/confirm-forgot-password?token=...
-    const isArgonautDomain = currentUrl.startsWith("http://localhost:3000/");
+    const currentUrl = window.location.href; 
+
+    // Các domain hợp lệ
+    const isArgonautDomain = currentUrl.startsWith("http://localhost:3000/") || currentUrl.startsWith("http://localhost:3001/") || currentUrl.startsWith("http://localhost:3002/") || currentUrl.startsWith(" https://argonaut.asia/");
+
     const urlParams = new URL(currentUrl).searchParams; // Parse URL
     const token = urlParams.get('token'); // Lấy token
     const navigate = useNavigate();
 
     if (!isArgonautDomain) {
-        // Nếu không phải domain chính thức, không xử lý
+        // Nếu không phải domain hợp lệ, không xử lý
         console.error("Truy cập từ domain không hợp lệ:", currentUrl);
         navigate('/404');
         return null;
